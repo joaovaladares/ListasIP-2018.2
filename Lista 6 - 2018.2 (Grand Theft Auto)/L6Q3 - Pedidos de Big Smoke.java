649cdcbffb51e19453ce2572ee5df33159bba860
João V. Valadares <42593399+joaovaladares@users.noreply.github.com>
2018-10-23 18:03:47 -0300

    Update L6Q3 - Pedidos de Big Smoke.java

--- Lista 6 - 2018.2 (Grand Theft Auto)/L6Q3 - Pedidos de Big Smoke.java
+++ Lista 6 - 2018.2 (Grand Theft Auto)/L6Q3 - Pedidos de Big Smoke.java
@@ -1,12 +1,29 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
-public class Pedidos_De_Big_Smoke {
-    public static void main(String[] args) {
+public class HuxleyCode {
+  public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-
+        ListaPedidos listaPedidos = new ListaPedidos();
+        String pedido = "";
+        while (!pedido.equals("The End")) {
+            pedido = in.nextLine();
+            if (!pedido.equals("The End")) {
+                listaPedidos.inserir(pedido);
+            }
+        }
+        while (in.hasNext()) {
+            pedido = in.nextLine();
+            if (pedido != null) {
+                listaPedidos.remover(pedido);
+            }
+        }
+        System.out.print(listaPedidos.toString());
+        if (listaPedidos.toString().equals("")) {
+            System.out.println("U gotta null pointer ma man");
+        }
     }
 }
-
 class ListaPedidos {
     private String pedido;
     private ListaPedidos proximo;
@@ -26,13 +43,18 @@ class ListaPedidos {
     }
 
     public void remover(String pedido) {
-        if (!this.pedido.equals("")) {
-            if (this.pedido.equals(pedido)) {
-                this.pedido = this.proximo.pedido;
-                this.proximo = this.proximo.proximo;
-            } else {
-                this.proximo.remover(pedido);
-            }
+        if (this.pedido.equals(pedido)) {
+            this.pedido = this.proximo.pedido;
+            this.proximo = this.proximo.proximo;
+        } else {
+            this.proximo.remover(pedido);
         }
+    }
+    public String toString() {
+        String resposta = "" + this.pedido;
+        if (this.proximo != null) {
+            resposta = resposta + "\n" + this.proximo.toString();
+        }
+        return resposta;
     }
 }
